f4b4ff73296d0c77367ae02524d6e722d8588082
Martin Froehlich <4023770+mjfroehlich@users.noreply.github.com>
2023-12-15 13:35:20 +0100

    TODO Failing test

--- Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
+++ Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
@@ -1,11 +1,34 @@
 package org.hyperskill.stopwatch
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val startButton: Button by lazy {  findViewById(R.id.startButton) }
+    private val resetButton: Button by lazy { findViewById(R.id.resetButton) }
+    private val textView: TextView by lazy { findViewById(R.id.textView) }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val vm: StopwatchViewModel by viewModels()
+        startButton.setOnClickListener {
+            vm.start()
+        }
+        resetButton.setOnClickListener {
+            vm.reset()
+        }
+
+        lifecycleScope.launch {
+            vm.state.collect {
+                textView.text = it.elapsed
+            }
+        }
     }
 }
--- Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/StopwatchViewModel.kt
+++ Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/StopwatchViewModel.kt
@@ -1,16 +1,74 @@
 package org.hyperskill.stopwatch
 
+import android.os.Looper
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-data class StopwatchState(
-    val isRunning: Boolean = false,
-    val startTime: Long = 0L,
-    val currentTime: Long = 0L,
+data class StopwatchViewState(
+    val elapsed: String = "00:00"
 )
 
 class StopwatchViewModel : ViewModel(){
-    private val _state = MutableStateFlow(StopwatchState())
-    val state: StateFlow<StopwatchState> = _state
+    private val _state = MutableStateFlow(StopwatchViewState())
+    val state: StateFlow<StopwatchViewState> = _state.asStateFlow()
+    val model = StopwatchModel()
+
+    private val handler = android.os.Handler(Looper.getMainLooper())
+
+    private val runnable = object : Runnable {
+        override fun run() {
+            _state.value = StopwatchViewState(elapsed())
+            handler.postDelayed(this, 1000)
+        }
+    }
+
+    fun start() {
+        if (model.isRunning) {
+            return
+        }
+        model.start()
+        _state.value = StopwatchViewState(elapsed())
+        handler.postDelayed(runnable, 1000)
+    }
+
+    fun reset() {
+        model.reset()
+        _state.value = StopwatchViewState(elapsed())
+        handler.removeCallbacks(runnable)
+    }
+
+    fun elapsed(): String {
+        val (min, sec) = model.elapsed()
+        return "%02d:%02d".format(min, sec)
+    }
+}
+
+class StopwatchModel {
+    private var startTime: Long = 0L
+    private var currentTime: Long = 0L
+    var isRunning: Boolean = false
+
+    fun start() {
+        startTime = System.currentTimeMillis()
+        isRunning = true
+    }
+
+    fun reset() {
+        startTime = 0L
+        currentTime = 0L
+        isRunning = false
+    }
+
+    fun elapsed(): Pair<Long, Long> {
+        val elapsed = if (isRunning) {
+            System.currentTimeMillis() - startTime
+        } else {
+            currentTime - startTime
+        }
+        val seconds = elapsed / 1000
+        val minutes = seconds / 60
+        return Pair(minutes, seconds % 60)
+    }
 }
